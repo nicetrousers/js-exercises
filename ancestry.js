66147de5dd22a0748987ffda10b87168162995ae
Keith Laidlaw <keithlaidlaw@gmail.com>
2017-12-01 14:16:03 -0800

    Add missing semicolon

--- ancestry.js
+++ ancestry.js
@@ -38,4 +38,7 @@ var ANCESTRY_FILE = JSON.stringify([
   {"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"},
   {"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"},
   {"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}
-])
+]);
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+console.log(ancestry);
