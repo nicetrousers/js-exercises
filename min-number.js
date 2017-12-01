748fd39bec789806c51348751950dc1fafec2c3f
Keith Laidlaw <keithlaidlaw@gmail.com>
2017-11-30 16:37:33 -0800

    Add title and description

--- min-number.js
+++ min-number.js
@@ -1,3 +1,9 @@
+/* Eloquent JavaScript ch03: Minimum
+
+The previous chapter introduced the standard function Math.min that
+returns its smallest argument. We can do that ourselves now. Write a
+function min that takes two arguments and returns their minimum. */
+
 function min(a, b) {
 	if ((a < 0) && (b < 0)) {
 		if (a / b < 1) {
