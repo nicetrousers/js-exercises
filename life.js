a709747782a28f12e8d01b7d67a5946bb5cec63c
Keith Laidlaw <keithlaidlaw@gmail.com>
2017-12-05 16:23:51 -0800

    automatically group all people by century born

--- life.js
+++ life.js
@@ -7,7 +7,12 @@ a closer look at that phenomenon.
 Compute and output the average age of the people in the ancestry data
 set per century. A person is assigned to a century by taking their
 year of death, dividing it by 100, and rounding it up, as in
-Math.ceil(person.died / 100). */
+Math.ceil(person.died / 100). 
+
+For bonus points, write a function groupBy that abstracts the grouping
+operation. It should accept as arguments an array and a function that
+computes the group for an element in the array and returns an object
+that maps group names to arrays of group numbers.*/
 
 function average(array) {
   function plus(a, b) { return a + b; }
@@ -45,45 +50,11 @@ ancestry.forEach(function(person) {
 	centuries[c].push(person);
 });
 
-console.log(centuries);
-
-	var ages = [];
-	var century = century;
-	ancestry.forEach(function(person) {
-		var c = Math.ceil(person.died / 100);
-		if (c === century) {
-			var age = person.died - person.born
+Object.keys(centuries).forEach(function(key) {
+	ages = [];
+	centuries[key].forEach(function(person) {
+		var age = person.died - person.born
 			ages.push(age);
-		}
 	});
 	console.log( average(ages) ); 
-};
-
-cAverage(19);
-
-
-
-/* For bonus points, write a function groupBy that abstracts the grouping
-operation. It should accept as arguments an array and a function that
-computes the group for an element in the array and returns an object
-that maps group names to arrays of group numbers. */
-
-function average(array) {
-  function plus(a, b) { return a + b; }
-  return array.reduce(plus) / array.length;
-}
-
-function cAverage(century) {
-	var ages = [];
-	var century = century;
-	ancestry.forEach(function(person) {
-		var c = Math.ceil(person.died / 100);
-		if (c === century) {
-			var age = person.died - person.born
-			ages.push(age);
-		}
-	});
-	console.log( average(ages) ); 
-};
-
-cAverage(19);
+});
