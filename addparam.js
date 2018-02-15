9a342079e8d5a1b5687c824bc4e8c82257218fac
Keith Laidlaw <keithlaidlaw@gmail.com>
2018-02-15 09:49:09 -0800

    get input html to convert to object and back to string

--- addparam.js
+++ addparam.js
@@ -1,42 +1,43 @@
 
 function addParams() {
-	var parameters = document.getElementById('parameters'); 
-	var codeInput = document.getElementById('codeInput');
+	var parameters = document.getElementById('parameters').value; 
+	var codeInput = document.getElementById('codeInput').value;
 
-	parameters = parameters.value;
-	codeInput = codeInput.value;
-
-	htmlStringToFrame(codeInput);
-
-
-	// var codeObject = document.getElementById('codeFrame');
-	var codeObject = document.getElementById('framewrap');
-
-	console.log(codeObject)
-	console.log(typeof(codeObject));
-
+	var codeObject = htmlStringToFrame(codeInput);
+  changeLinks(codeObject, parameters);
 	var codeOutput = frameToHtmlString(codeObject);
-
-	console.log(codeOutput)
-	console.log(typeof(codeOutput));
-
 	document.getElementById('codeOutput').innerHTML = codeOutput;
-	// document.getElementById('codeOutput').innerHTML = codeObject;
-	return false;
+	removeFrame(codeObject);
+
+	// return false;
 }
 
 function htmlStringToFrame(html) {
-  var frame = document.getElementById('codeFrame');
+  var frame = document.createElement('iframe');
+  frame.style.display = 'none';
+  document.body.appendChild(frame);
   frame.contentDocument.open();
   frame.contentDocument.write(html);
   frame.contentDocument.close();
+  return frame;
 }
 
 function frameToHtmlString(frame) {
-  // var codeOutput = frame.contentDocument.body.innerHTML;
-  var codeOutput = frame.contentDocument.innerHTML;
-
-  // var codeOutput = frame.innerHTML;
-
+	var codeOutput = frame.contentDocument.documentElement.outerHTML;
   return codeOutput;
+}
+
+function removeFrame(frame) {
+	document.body.removeChild(frame);
+};
+
+function changeLinks(frame,parameters) {
+	var anchorList = frame.contentDocument.getElementsByTagName('a');
+	for (i = 0; i < anchorList.length; i++) {
+		var anchor = anchorList[i];
+	  anchor = (anchorList[i].href.split('?')[0] + parameters);
+	  console.log(anchor);
+	  anchorList[i].setAttribute('href', anchor);
+	};
+	return frame;
 }
