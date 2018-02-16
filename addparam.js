54c799e76ed9c4bdef9483e2308d8468203b3550
Keith Laidlaw <keithlaidlaw@gmail.com>
2018-02-16 13:36:53 -0800

    add clearer user feedback and variable url exceptions

--- addparam.js
+++ addparam.js
@@ -1,15 +1,13 @@
-
 function addParams() {
-	var parameters = document.getElementById('parameters').value; 
+	var parameters = document.getElementById('parameters').value.trim(); 
 	var codeInput = document.getElementById('codeInput').value;
 
+	parameters = checkParameters(parameters);
 	var codeObject = htmlStringToFrame(codeInput);
   changeLinks(codeObject, parameters);
 	var codeOutput = frameToHtmlString(codeObject);
 	document.getElementById('codeOutput').innerHTML = codeOutput;
-	removeFrame(codeObject);
-
-	// return false;
+	document.body.removeChild(codeObject)
 }
 
 function htmlStringToFrame(html) {
@@ -27,17 +25,44 @@ function frameToHtmlString(frame) {
   return codeOutput;
 }
 
-function removeFrame(frame) {
-	document.body.removeChild(frame);
-};
-
-function changeLinks(frame,parameters) {
+function changeLinks(frame,params) {
 	var anchorList = frame.contentDocument.getElementsByTagName('a');
 	for (i = 0; i < anchorList.length; i++) {
-		var anchor = anchorList[i];
-	  anchor = (anchorList[i].href.split('?')[0] + parameters);
-	  console.log(anchor);
-	  anchorList[i].setAttribute('href', anchor);
+		var anchor = anchorList[i].getAttribute('href');
+
+		if (anchor.indexOf('%%') == -1) {
+			if (anchor.indexOf('?') != -1) anchor = (anchor + "&" + params);
+			else anchor = (anchor + "?" + params);
+	 
+		  console.log(anchor); // remove when done
+		  printOutput(anchor, 'outputLinks');
+
+		  anchorList[i].setAttribute('href', anchor);
+		} else {
+			// anchor = anchorList[i].href.value;
+			printOutput(anchor, 'notOutputLinks');
+		}
 	};
 	return frame;
+}
+
+function checkParameters(params) {
+	if (params.length < 1) {
+		alert("please enter parameters")
+		return false
+	} else {
+		if (params.charAt(0) === "?") {
+	    params = params.slice(1);
+		}
+		params = params.replace(/\s/g, '');
+		printOutput(params,'outputParams');
+		return params;
+	}
+}
+
+function printOutput(text,target) {
+	var target = target;
+	var outputTarget = document.getElementById(target);
+	var outputText = "<p class='code'>"+text+"</p>";
+	outputTarget.innerHTML += outputText;
 }
